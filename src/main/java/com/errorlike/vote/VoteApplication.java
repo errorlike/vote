ed96f6081e573345cf8be828bff9344bd46ae4d4
errorlike <errorlikeman@outlook.com>
2023-01-19 00:50:39 +0800

    fake users

--- src/main/java/com/errorlike/vote/VoteApplication.java
+++ src/main/java/com/errorlike/vote/VoteApplication.java
@@ -1,12 +1,42 @@
 package com.errorlike.vote;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.errorlike.vote.entities.User;
+import com.errorlike.vote.repositories.UserRepository;
+import com.github.javafaker.Faker;
 
 @SpringBootApplication
 public class VoteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VoteApplication.class, args);
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(VoteApplication.class, args);
-	}
+    @Bean
+    Faker faker() {
+        return new Faker();
+    }
+
+    @Bean
+    CommandLineRunner dataLoader(Faker faker, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+
+        return args -> {
+            if (userRepository.count() <= 100) {
+                List<User> users = IntStream.rangeClosed(1, 100)
+                        .mapToObj(i -> new User(null, faker.name().fullName(), passwordEncoder.encode("123456"),
+                                faker.random().nextInt(1, 120),
+                                faker.internet().emailAddress(), LocalDateTime.now().withNano(0)))
+                        .toList();
+                userRepository.saveAll(users);
+            }
+        };
+    }
 }
