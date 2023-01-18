834c5d45ce56cd3a12c36452914cd54f3e061d87
errorlike <errorlikeman@outlook.com>
2023-01-18 23:27:31 +0800

    add UserController
    add users endpoint,can paging
    
    add users/id endpoint

---
+++ src/main/java/com/errorlike/vote/controllers/UserController.java
@@ -0,0 +1,40 @@
+package com.errorlike.vote.controllers;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.errorlike.vote.entities.User;
+import com.errorlike.vote.repositories.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/users")
+public class UserController {
+    private final UserRepository userRepository;
+
+    @GetMapping()
+    public ResponseEntity<?> getUsers(@RequestParam(defaultValue = "0") Integer page,
+            @RequestParam(defaultValue = "5") Integer size) {
+        PageRequest paging = PageRequest.of(page, size);
+        Page<User> users = userRepository.findAll(paging);
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getUserById(@PathVariable String id) {
+
+        Optional<User> user = userRepository.findById(Long.valueOf(id));
+        return ResponseEntity.ok(user.orElseThrow());
+    }
+
+}
