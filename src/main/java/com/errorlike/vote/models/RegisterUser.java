d72c5fcb661dd82ffa647c607d54c7e5e86b2a13
errorlike <errorlikeman@outlook.com>
2023-01-30 15:29:53 +0800

    fix @size cannot use for integer type

--- src/main/java/com/errorlike/vote/models/RegisterUser.java
+++ src/main/java/com/errorlike/vote/models/RegisterUser.java
@@ -1,8 +1,9 @@
 package com.errorlike.vote.models;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
 
 import lombok.Data;
 
@@ -21,6 +22,7 @@ public class RegisterUser {
     private String email;
 
     @NotNull
-    @Size(min = 0, max = 120)
+    @Min(0)
+    @Max(120)
     private Integer age;
 }
