5cdc864db3d4ac3fe507372835f0295949f4c3f0
errorlike <errorlikeman@gmail.com>
2023-02-05 22:13:19 +0800

    fix path add DataIntegrityViolationException catch

--- src/main/java/com/errorlike/vote/controllers/AuthenticationController.java
+++ src/main/java/com/errorlike/vote/controllers/AuthenticationController.java
@@ -5,11 +5,7 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
-import com.errorlike.vote.entities.User;
-import com.errorlike.vote.models.RegisterUser;
-import com.errorlike.vote.models.loginRequest;
-import com.errorlike.vote.services.AuthenticationService;
-
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,12 +13,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.errorlike.vote.entities.User;
+import com.errorlike.vote.models.RegisterUser;
+import com.errorlike.vote.models.loginRequest;
+import com.errorlike.vote.services.AuthenticationService;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 @Slf4j
 public class AuthenticationController {
@@ -38,8 +40,13 @@ public class AuthenticationController {
                 .email(registerUser.getEmail())
                 .createTime(LocalDateTime.now().withNano(0))
                 .build();
-        log.info(passwordEncoder.encode(registerUser.getPassword()));
-        authenticationService.register(user);
+        try {
+            authenticationService.register(user);
+
+        } catch (DataIntegrityViolationException e) {
+            log.error(e.getMessage());
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "重复的用户名或邮箱", e);
+        }
         // todo chang body content to message
         return ResponseEntity.status(HttpStatus.CREATED).body(user);
     }
