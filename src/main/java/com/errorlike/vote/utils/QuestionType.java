e3fe1bf65458d1cccf2c765c8cf3e41aad64a88b
errorlike <errorlikeman@gmail.com>
2023-02-27 02:03:45 +0800

    modify questionType can convert json

--- src/main/java/com/errorlike/vote/utils/QuestionType.java
+++ src/main/java/com/errorlike/vote/utils/QuestionType.java
@@ -1,11 +1,15 @@
 package com.errorlike.vote.utils;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter(onMethod = @__(@JsonValue))
 public enum QuestionType {
     RADIO(1),
     MULTI(2);
-    public final int questionValue;
+    public final Integer questionValue;
 }
